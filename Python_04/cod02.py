831d785d5408f0da87baab1672a6f96aa5e78f8d
Lucas Mazoni Guerra <126295421+lumguerra@users.noreply.github.com>
2023-03-29 20:30:26 -0300

    Create cod02.py

---
+++ Python_04/cod02.py
@@ -0,0 +1,8 @@
+#Operadores de atribuição
+num=int(input("Digite um número: "))#num=3
+x=5
+print("valores iniciais, num=",num,"x=",x)
+num+=1#num=3+1 num=4
+num+=x#num=4+5 num=9
+x-=2#x=5-2 x=3
+print("valores após atribuições, num=", num,"x=",x)
