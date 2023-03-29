c8b5add0b425cb3d849b035b754fac0c9b6db48d
Lucas Mazoni Guerra <126295421+lumguerra@users.noreply.github.com>
2023-03-29 20:30:05 -0300

    Create cod01.py

---
+++ Python_04/cod01.py
@@ -0,0 +1,25 @@
+#Operadores aritméticos
+x = int(input("Digite o primeiro número "))
+y = int(input("Digite o segundo número "))
+print("Soma:",x+y)
+print("Subtração:",x-y)
+print("Multiplicação:",x*y)
+print("Divisão:",x/y)
+print("Divisão inteira:",x//y)
+print("Resto da Divisão:",x%y)
+print("Exponenciação:",x**y)
+
+idade1=int(input("digite a idade 1: "))
+idade2=int(input("digite a idade 2: "))
+print("Soma: ",idade1+idade2)
+print("Subtração: ",idade1-idade2)
+print("Multiplicação: ",idade1*idade2)
+print("Divisão: ",idade1/idade2)
+print("Divisão inteira: ",idade1//idade2)
+print("Resto da divisão: ",idade1%idade2)
+print("Exponenciação: ",idade1**idade2)  
+
+'''
+O significado do sinal = , significa atribuição
+
+'''
