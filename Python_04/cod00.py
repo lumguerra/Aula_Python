776d717f6f074c2fc297c4c7cee57e7055f51679
Lucas Mazoni Guerra <126295421+lumguerra@users.noreply.github.com>
2023-03-29 20:29:39 -0300

    Create cod00.py

---
+++ Python_04/cod00.py
@@ -0,0 +1,15 @@
+#Calcular a média de 3 notas
+nota1=float(input("Digite a nota 1: "))
+nota2=float(input("Digite a nota 2: "))
+nota3=float(input("Digite a nota 3: "))
+media=(nota1+nota2+nota3)/3
+print("media: ",media)
+
+'''
+resto da divisão= % é o valor restante de uma divisão 
+todo numero impar, quando calculado o resto da divisão por 2 = 1
+todo numero par, quando calculado o resto da divisao  por 2= 0
+exemplo:
+o resto da divisao de 16547=1
+o resto da divisao de 1325168=0
+'''
