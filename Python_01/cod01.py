6d65ed47bbc380f0a0523d8ead6c608c6ff216de
Lucas Mazoni Guerra <126295421+lumguerra@users.noreply.github.com>
2023-03-29 20:18:39 -0300

    Add files via upload

---
+++ Python_01/cod01.py
@@ -0,0 +1,48 @@
+caixa="pratos"
+print(caixa)
+ano="2023"
+print(ano)
+unidade="Vergueiro"
+print(unidade)
+variavel="conteúdo da variável"
+print (variavel)
+print ("Alo mundo!")
+#print ("Meu nome é Lucas")
+n1=20
+print(n1)
+x=30
+print(x)
+#1y=10
+#print(1y)
+#NAO SE CRIA VARIAVEL COMEÇANDO POR NÚMERO
+nome_completo="Lucas Mazoni Guerra"
+print (nome_completo)
+#não se coloca espaços na variável, para isso se usa o underscore _ ou letra maiúscula.
+print ("Estou aprendendo a programar")
+print ("Esse é o meu primeiro código escrito em Phyton") #Teste de comentário
+#Para escrever um comentário no Python é necessário adicionar "#", mas o "#" também pode ser usado para desativar uma linha de código
+"""
+Resumo da aula:
+Algoritmo, descrição narrativa,
+Na primeira aula aprendemos que o código é composto por input, processamento e output
+fluxograma: descrição do algoritmo de forma visual
+Na aula de hoje aprendemos:
+-O replit
+-Imprimir frases na tela usando o Phyton
+-Adicionar comentários de uma linha no código
+-Desativar uma linha de código usando a hashtag
+-Adicionar um comentário usando várias linhas nó código com três aspas duplas abrindo o comentário e três aspas duplas fechando o comentário.
+-Variáveis: Local de armazenamento de dados
+-As variáveis devem ser escritas seguindo algumas regras
+"""
+
+#comentário
+print("relembrando a aula passada:")
+print("para printar texto coloque parenteses e aspas com o texto dentro")
+nome="lucas mazoni guerra"
+print(nome)
+teste="teste de variavel"
+print(teste)
+print ("teste")
+
+
